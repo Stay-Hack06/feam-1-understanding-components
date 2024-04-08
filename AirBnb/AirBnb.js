22c539c4959bfa6a79cc63bc34aa922a24ddcd6c
Stay-Hack <staysha.l.hackmann@gmail.com>
2024-04-08 12:36:38 -0700

    complete

--- AirBnb/AirBnb.js
+++ AirBnb/AirBnb.js
@@ -1,39 +1,40 @@
 /**
  Using the image of the Airbnb UI, answer the following questions:
  1. What are four main components you see on the page?
-
-
-
+Header
+Search Bar
+Listing Cards
+Filters
 
 
  2. Put the components in order to show the parent/child relationship. So if component x is the mother of y and grandmother of z, you would write: 
     1. x
     2. y
     3. z
-
+1. App
+  2. Header
+    3. Search Bar
+      4. Filters
+        5. Listing Cards
 
 
 
 3. Which component needs access to every piece of data? Which component only needs access to the filtered data?
 
-
-
-
-
+The component that needs access to every piece of data is likely the parent component that manages the state of all listings, App. 
+The component that only needs access to the filtered data is the Listing Cards component that displays the filtered listings.
 
 
 4. Think about what data we would need to know for each Airbnb listing. Recall that a Javascript object is a collection of key/value pairs. 
 
 What are three different key/value pairs that will be needed for each listing?
 
-
-
-
-
-
-
+id: unique identifier
+title: name of the listing
+location: City, State
 
 5. Let's say we want to only see Airbnb listings that have a pool. In other words, we want to filter out all of the listings that do not have pools. Can you write some pseudo-code to accomplish this? 
+
 
 Below is an example of Airbnb listing data that we would work with. 
  */
@@ -64,3 +65,6 @@ airbnbListings = [
     lakeFront: false,
   },
 ];
+
+const poolListings = airbnbListings.filter(listing => listing.hasPool);
+console.log(poolListings);
--- calculator-am1/QUESTIONS.md
+++ calculator-am1/QUESTIONS.md
@@ -2,10 +2,10 @@
 
 **Answer the following questions**
 
-1. Which folder holds the components?
-2. Which component is the parent of all other components?
-3. Where would you add a folder for images? Another way to ask is, which folder will be the parent of the images folder?
-4. Does App have any grandchildren? How do you know?
-5. What do you notice about the css files?
-6. How are the css files connected to the js files? (Look at the top of the js files for clues)
-7. Look for an HTML file. What do you notice about it? 
+1. Which folder holds the components? src/Components 
+2. Which component is the parent of all other components? App
+3. Where would you add a folder for images? Another way to ask is, which folder will be the parent of the images folder? src/Components 
+4. Does App have any grandchildren? How do you know? yes, button. Because button is the child of botton box
+5. What do you notice about the css files? not sure what I'm supposed to notice other than them being specific to the container
+6. How are the css files connected to the js files? (Look at the top of the js files for clues) through the container components 
+7. Look for an HTML file. What do you notice about it? It's letting react write what would have been written out in HTML
